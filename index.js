40c9488c793a13374c884466786baaa5811d6344
Carlos Cortes <carloscortesmora@gmail.com>
2020-05-31 12:03:30 -0400

    finished auth flow

--- index.js
+++ index.js
@@ -1,9 +1,42 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+const keys = require('./config/keys');
+require('./models/User');
+require('./services/passport');
+
+const conectarDB = async() => {
+    try {
+        await mongoose.connect(keys.mongoURI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useFindAndModify: false,
+            useCreateIndex: true
+        })
+        console.log('DB Conectada');
+    } catch (error) {
+        console.log('Hubo un error');
+        console.log(error);
+        process.exit(1);
+    }
+}
+
+conectarDB();
+
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send({ bye: 'buddy' });
-});
+app.use(
+    cookieSession({
+        maxAge: 30 * 24 * 60 * 60 * 1000,
+        keys: [keys.cookieKey]
+    })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+require('./routes/authRoutes')(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
